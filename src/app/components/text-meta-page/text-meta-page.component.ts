0a6323f2cfdb95c821b57eeab66ff5eba6f19817
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2025-06-05 17:06:58 +0200

    added : button to create a prepare a paragraph as training sets

--- src/app/components/text-meta-page/text-meta-page.component.ts
+++ src/app/components/text-meta-page/text-meta-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HoverAblesContainerComponent } from '../hover-ables-container/hover-ables-container.component';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-text-meta-page',
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
       <!-- <input type="text" [(ngModel)]="text"> -->
       <textarea name="paragraphInput" [(ngModel)]="text"></textarea>
     </label>
+    <button (click)="CreateNewSets()">Create New Sets</button>
     <p><app-hover-ables-container [text]="text"></app-hover-ables-container></p>
   `,
   styles: [
@@ -24,4 +26,10 @@ import { FormsModule } from '@angular/forms';
 })
 export class TextMetaPageComponent {
   text:string = '';
+  private router = inject(Router);
+
+  CreateNewSets() {
+    const newSet = new Set(this.text.split("").filter(c=>c.match('[\u4e00-\u9faf]')?.length ?? false));
+    this.router.navigate(["trainingset"],{ queryParams:{ new: encodeURIComponent([...newSet].join("")) }})
+  }
 }
